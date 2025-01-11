41d23af33e41fffad25f85c24ccf747427440031
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-01-11 21:02:25 +0530

    init

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+
+  return (
+    <View><Text>Hiii ashu</Text></View>
+  );
+}
+
+
+
+export default App;

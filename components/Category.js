9ebfb10320f0b6c6d2d63cdc95d79bba96661f95
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-01-12 13:23:39 +0530

    category underline and text change updatedd

--- components/Category.js
+++ components/Category.js
@@ -3,35 +3,45 @@ import { fontSize, spacing } from "../constants/dimensions"
 import { fontFamily } from "../constants/fonts"
 import { colors } from "../constants/colors"
 import { category } from "../data/category"
+import { useState } from "react"
 
 const Category = () => {
-    return (
-    <FlatList data={category} renderItem={({item,index})=>(
-        <TouchableOpacity>
-        <Text style={styles.categoryText}>{item.name}</Text>
-        <View style={styles.underline}/>
-        </TouchableOpacity>
-    )}
-    keyExtractor={(item)=>item.id}
-    horizontal
-    ItemSeparatorComponent={
-<View style={{paddingHorizontal:spacing.sm}}/>
+    const [selectedCategory, setSelectedCategory] = useState("Smart Watch")
+    const handleSelectedCategory = (category) => {
+        setSelectedCategory(category)
     }
-    />
+    return (
+        <FlatList data={category} renderItem={({ item, index }) => (
+            <TouchableOpacity onPress={() => { setSelectedCategory(item.name) }}>
+                <Text style={[styles.categoryText, selectedCategory===item.name&&{color:colors.purple}]}>{item.name}</Text>
+                {
+                    (selectedCategory === item.name) ? <View style={styles.underline} /> : null
+
+
+                }
+            </TouchableOpacity>
+        )}
+            keyExtractor={(item) => item.id}
+            horizontal
+            ItemSeparatorComponent={
+                <View style={{ paddingHorizontal: spacing.sm }} />
+            }
+
+        />
     )
 }
 
 const styles = StyleSheet.create({
-categoryText:{
-    fontSize:fontSize.md,
-    fontFamily:fontFamily.SemiBold,
-    color:colors.grey
-},
-underline:{
-    borderBottomColor:colors.purple,
-    borderBottomWidth:2,
-    width:"50%",
-    marginTop:spacing.sm
-}
+    categoryText: {
+        fontSize: fontSize.md,
+        fontFamily: fontFamily.SemiBold,
+        color: colors.grey
+    },
+    underline: {
+        borderBottomColor: colors.purple,
+        borderBottomWidth: 2,
+        width: "50%",
+        marginTop: spacing.sm
+    }
 })
 export default Category

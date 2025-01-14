d08720e0bb826770cda0956c94a65a0961b67596
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-01-14 23:40:55 +0530

    color selector added

--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,19 +1,39 @@
 import { useRoute } from "@react-navigation/native"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from "react-native"
 import Header from "../components/Header"
 import ProductImages from "../components/ProductImages"
 import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
 import { fontSize, iconSize, spacing } from "../constants/dimensions";
 import { colors } from "../constants/colors";
 import { fontFamily } from "../constants/fonts";
+import { useState } from "react"
 
 
 
+const colorData = [
+    {
+        colorName: "Silver",
+        colorValue: "#F0F2F2"
+
+    },
+    {
+        colorName: "Bright Oragne",
+        colorValue: "#F25745"
+
+    },
+    {
+        colorName: "Starlight",
+        colorValue: "#FAF6F2"
+
+    }
+]
 
 
 const ProductDetailsScreen = () => {
     const item = useRoute().params.item
     console.log(item.images[0])
+
+    const [selectedColor, setSelectedColor] = useState("")
 
     return (
         <View>
@@ -32,6 +52,29 @@ const ProductDetailsScreen = () => {
 
                 </View>
             </View>
+
+            <View style={styles.colorContinaer}>
+                <Text style={styles.colorsHeading}>Colors</Text>
+                <FlatList data={colorData} renderItem={({ item }) => {
+                    return (
+
+                        <TouchableOpacity onPress={() => { setSelectedColor(item.colorValue) }} 
+                        style={[styles.selectColorContainer, item.colorValue === selectedColor && { borderColor: colors.purple }]}>
+                            <View style={[styles.circleColor, { backgroundColor: item.colorValue }]} />
+
+
+                            <Text style={styles.colorText}>{item.colorName}</Text>
+                        </TouchableOpacity>
+                    )
+                }}
+                    horizontal
+                    ItemSeparatorComponent={() => (<View
+                        style={{ width: spacing.sm }}
+                    />)}
+                />
+
+
+            </View>
         </View>
 
     )
@@ -45,7 +88,7 @@ const styles = StyleSheet.create({
     },
     titleWrapper: {
         flex: 1,
-        paddingHorizontal:spacing.md
+        paddingHorizontal: spacing.md
     },
     productTitle: {
         fontFamily: fontFamily.Bold,
@@ -74,7 +117,41 @@ const styles = StyleSheet.create({
         color: colors.gray,
         fontFamily: fontFamily.Medium,
         fontSize: fontSize.md,
+    },
+    colorContinaer: {
+        paddingTop: spacing.md,
+        margin: spacing.md
+    },
+    colorsHeading: {
 
+        fontSize: fontSize.md,
+        fontFamily: fontFamily.SemiBold,
+        color: colors.black,
+        paddingBottom: spacing.md
+    },
+    selectColorContainer: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: colors.gray,
+        borderRadius: 4,
+        padding: spacing.sm,
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    circleColor: {
+        height: spacing.md,
+        width: spacing.md,
+        backgroundColor: colors.purple,
+        borderRadius: spacing.md / 2,
+        marginEnd: spacing.sm
+
+    },
+    colorText: {
+        fontSize: fontSize.sm,
+        fontFamily: fontFamily.Medium,
+        color: colors.black
 
     }
 
